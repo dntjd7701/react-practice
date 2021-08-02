3b917eab851f886be48032d1764ec7fd815cfbc8
dntjd7701 <dntjd7701@gmail.com>
2021-08-02 13:51:52 +0900

    [react]: kanban board tnwjd

--- kanban/src/ex03/Card.js
+++ kanban/src/ex03/Card.js
@@ -1,27 +1,31 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import TaskList from './TaskList';
 import styles from '../assets/scss/Card.scss';
 
-export default function Card({ title, description, status, tasks}){
-    const [showDetails, setShowDetails] = useRef(true);
+export default function Card({ title, description, status, tasks }){
+    const [showDetails, setShowDetails]  = useState(false);
     const styleSideColor={
         position:'absolute',
         top:0,
         left:0,
         bottom:0,
         width:3,
-        backgroundColor: status == 'ToDo' ? 'purple' : status == 'Doing' ? 'deeppink' : "skyblue"
+        backgroundColor: status === 'ToDo' ? 'purple' : status === 'Doing' ? 'deeppink' : "skyblue"
     }
     return (
-        <div className= { styles.Card }>
-            <div style= { styleSideColor }/>
+        <div className= { styles.Card } >
+            <div style= { styleSideColor } />
                 <div
                     className=
-                    { showDetails ? [styles.Card__Title, styles.Card__Title__open].join('') : styles.Card__Title }
-                    onClick={ e => { setShowDetails(!showDetails )}}>{ title }
+                    { showDetails ? [styles.Card__Title, styles.Card__Title__open].join(' ') : styles.Card__Title }
+                    onClick={ () => { setShowDetails(!showDetails )} } > { title }
                 </div>
-            { showDetails ? <div className={styles.Card__Details} > {description} <TaskList tasks={ tasks }/> </div> : null }
-
+            {
+                showDetails ?
+                    <div className={styles.Card__Details}> {description} <TaskList tasks={ tasks }/>
+                    </div>
+                    : null
+            }
         </div>
     );
 };  
